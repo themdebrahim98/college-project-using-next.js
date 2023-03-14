5f3c79cca8175b2ce2bf5356d4acb7009cebdaf7
i-am-debanjan <debanjan16189251@gmail.com>
2023-03-14 14:47:47 +0530

    notice_added

--- pages/account.js
+++ pages/account.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Grid, Stack, TextField, Button, IconButton,Container,Typography, Card } from "@mui/material";
 import BaseCard from "../src/components/baseCard/BaseCard";
 import { useSelector } from 'react-redux';
+import { commonConstants } from '../src/constant/common.constant';
 function account() {
 const data = useSelector((store)=>store.user);
-console.log(data);
+console.log(data.userData.user_data);
   return (
     <Container>
       <Grid container spacing={0}>
@@ -18,24 +19,42 @@ console.log(data);
               Account Details
             </Typography>
             <Stack spacing={1}>
+            {data.userData.user_data.type == commonConstants.userTypeAdmin ?<>
             <Typography
               variant="h4"
             //   fontWeight="700"
             >
-              UserID : STDNT_10000120079
+              Name : {data.userData.user_data.id}
             </Typography>
             <Typography
               variant="h4"
             //   fontWeight="700"
             >
-              Name : Debanjan
+              Type : {data.userData.user_data.type}
+            </Typography></>:<><Typography
+              variant="h4"
+            //   fontWeight="700"
+            >
+              <b>UserId</b> : {data.userData.user_data.id}
             </Typography>
             <Typography
               variant="h4"
             //   fontWeight="700"
             >
-              Email : deban@16189251@gmail.com
+              <b>Name</b> : {data.userData.user_data.first_name+" "+data.userData.user_data.last_name}
             </Typography>
+            <Typography
+              variant="h4"
+            //   fontWeight="700"
+            >
+             <b>Gender</b> : {data.userData.user_data.gender}
+            </Typography>
+            <Typography
+              variant="h4"
+            //   fontWeight="700"
+            >
+              <b>Email</b> : {data.userData.user_data.email_address}
+            </Typography></>}
             </Stack>
             {/* <Grid item xs={12} sx={{ mt: 4 }} lg={6}>
               <Button variant="contained" color="primary" >
---
+++ pages/notices/index.js
@@ -0,0 +1,151 @@
+import { Grid, Button,
+  Typography,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Chip,
+} from "@mui/material";
+import React, { useState, useEffect } from "react";
+import BaseCard from "../../src/components/baseCard/BaseCard";
+import { BASE_URL } from "../../commonVariable";
+import NextLink from "next/Link";
+import Cookies from "js-cookie";
+import axios from "axios";
+function notices() {
+  const [noticeData, setnoticeData] = useState([]);
+  const btnData = (
+    <NextLink href="/notices/addNotice">
+      <Button variant="contained" sx={{ ml: "auto" }}>
+        Add Notice
+      </Button>
+    </NextLink>
+  );
+  useEffect(() => {
+     const token = Cookies.get("access_key");
+
+    const fetchNotice = async () => {
+      const res = await axios.post(`${BASE_URL}get_all_notice`, null, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      console.log(res.data.data.notices);
+      const allNotices = res.data.data.notices.map((elm, idx) => {
+        return elm;
+      });
+      // console.log(allTeachers, "allkjdscnkvn");
+      setnoticeData(allNotices);
+    };
+    fetchNotice();
+  },[]);
+  console.log(Cookies.get("access_key"));
+ 
+  const products = [
+    {
+      id: "1",
+      title: "Sunil Joshi",
+      views: "Web Designer",
+    },
+    {
+      id: "2",
+      title: "Sunil Joshi",
+      views: "Web Designer",
+    },
+    {
+      id: "3",
+      title: "Sunil Joshi",
+      views: "Web Designer",
+    },
+    {
+      id: "4",
+      title: "Sunil Joshi",
+      views: "Web Designer",
+    },
+  ];
+  return (
+    <BaseCard
+      title="Notice List"
+      button="true"
+      buttonData={btnData}
+      sx={{ overFlow: "scroll" }}
+    >
+      <Table
+          aria-label="simple table"
+          sx={{
+            mt: 3,
+            whiteSpace: "nowrap",
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography color="textSecondary" variant="h6">
+                  Id
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="textSecondary" variant="h6">
+                  Tittle
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography color="textSecondary" variant="h6">
+                  Action
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {noticeData.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell>
+                  <Typography
+                    sx={{
+                      fontSize: "15px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    {product.id}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Box>
+                      <Typography
+                        sx={{
+                          fontSize: "13px",
+                        }}
+                      >
+                        {product.title}
+                      </Typography>
+                    </Box>
+                  </Box>
+                </TableCell>
+                <TableCell>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Box>
+                      <NextLink href={"/notices/"+product.id}><Button variant="contained" color="success">View</Button></NextLink>
+                    </Box>
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+    </BaseCard>
+  );
+}
+
+export default notices;
