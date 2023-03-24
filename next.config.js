855b492527701993a56258267e6b55b876abdd37
mdebrahim98 <mdebrahim742148@gmail.com>
2023-03-24 15:33:15 +0530

    .next.config changed

--- next.config.js
+++ next.config.js
@@ -4,9 +4,17 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   register: true,
   scope: '/',
+  
 })
 
 module.exports = withPWA({
+  images: {
+   
+    cacheDuration: 365 * 24 * 60 * 60, // cache for 1 year
+    path: '/_next/image-cache/',
+    deviceSizes: [320, 640, 750, 1080, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+  },
   // next.js config
 })
 // const nextConfig = {
