6c49fa797009109b56c5ef1e5f76d0e8d1da23a4
mdebrahim98 <mdebrahim742148@gmail.com>
2023-04-14 17:39:01 +0530

    next.config bugs solved

--- next.config.js
+++ next.config.js
@@ -15,3 +15,22 @@
 // // }
 
 // // // module.exports = nextConfig
+
+
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/',
+    
+  })
+  
+  module.exports = withPWA({
+    // next.js config
+  })
+  // const nextConfig = {
+  //   reactStrictMode: true,
+  // }
+  
+  // // module.exports = nextConfig
